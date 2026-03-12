95485fb81fe4dc7059dba37f3e3c0700111256ec
Jonathan Youyoutte <27676720+jyouyoutte@users.noreply.github.com>
2026-03-12 20:00:04 +0100

    Init 1st component incident

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Incidents } from './features/incidents/incidents';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: 'incidents', component: Incidents },
+	{ path: '', redirectTo: 'incidents', pathMatch: 'full' }
+];
